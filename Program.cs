7cc491a8a887f74700d3f7d33552cfcca595ba22
tiunov <qwestern13@yandex.ru>
2025-07-03 17:14:29 +0500

    Added translit file names

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 
 class Program
 {
@@ -13,7 +14,7 @@ class Program
             Console.WriteLine("Please specify the path to the directory with MP4 files");
             return;
         }
-        
+
         string folder = args[0];
 
         if (!Directory.Exists(folder))
@@ -21,18 +22,18 @@ class Program
             Console.WriteLine("Folder doesn't exist!");
             return;
         }
-       
+
         var mcmFiles = Directory.GetFiles(folder, "*.mcm");
         if (mcmFiles.Length > 0)
         {
             foreach (var file in mcmFiles)
             {
                 var originalFileName = Path.GetFileNameWithoutExtension(file);
-                var outputFile = folder + "/" +  originalFileName;
+                var outputFile = folder + "/" + originalFileName;
                 ConvertToMp4(file, outputFile);
-            } 
+            }
         }
-        
+
         var mp4Files = Directory.GetFiles(folder, "*.mp4");
         if (mp4Files.Length == 0)
         {
@@ -42,13 +43,23 @@ class Program
         {
             foreach (var file in mp4Files)
             {
+                var originalFileName = Path.GetFileName(file);
+                var tranlitedFileName = TranslitFileName(originalFileName);
+                System.IO.File.Move(folder + "/" + originalFileName, folder + "/" + tranlitedFileName);
+            }
+            
+            var mp4FilesAfterTranslit = Directory.GetFiles(folder, "*.mp4");
+            
+            foreach (var file in mp4FilesAfterTranslit)
+            {
                 StartRtspStream(file, port);
             }
+
             System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
         }
 
     }
-    
+
     static void ConvertToMp4(string inputFile, string outputFile)
     {
         var processStartInfo = new ProcessStartInfo
@@ -60,7 +71,7 @@ class Program
             UseShellExecute = false,
             CreateNoWindow = true
         };
-        
+
         var process = new Process { StartInfo = processStartInfo };
         process.Start();
         process.WaitForExit();
@@ -73,7 +84,8 @@ class Program
         var processStartInfo = new ProcessStartInfo
         {
             FileName = "ffmpeg",
-            Arguments = $"-re -stream_loop -1 -i \"{inputFile}\" -c:v libx264 -preset ultrafast -pix_fmt yuv420p -c:a copy -movflags +faststart -f rtsp rtsp://rtsp-server:{port}/{filename}",
+            Arguments =
+                $"-re -stream_loop -1 -i \"{inputFile}\" -c:v libx264 -preset ultrafast -pix_fmt yuv420p -c:a copy -movflags +faststart -f rtsp rtsp://rtsp-server:{port}/{filename}",
             RedirectStandardOutput = false,
             RedirectStandardError = false,
             UseShellExecute = false,
@@ -83,6 +95,45 @@ class Program
         process.Start();
 
         Console.WriteLine($"Stream started: rtsp://localhost:{port}/{filename}");
-        
+
     }
+
+    static string TranslitFileName(string fileName)
+    {
+        StringBuilder translitedFileName = new StringBuilder();
+
+        foreach (char c in fileName)
+        {
+            if (TranslitMap.ContainsKey(c))
+            {
+                translitedFileName.Append(TranslitMap[c]);
+            }
+            else
+            {
+                translitedFileName.Append(c);
+            }
+        }
+
+        return translitedFileName.ToString();
+
+    }
+    
+    private static readonly Dictionary<char, string> TranslitMap = new Dictionary<char, string>
+    {
+        {'а', "a"},   {'б', "b"},   {'в', "v"},   {'г', "g"},   {'д', "d"},
+        {'е', "e"},   {'ё', "e"},   {'ж', "zh"},  {'з', "z"},   {'и', "i"},
+        {'й', "y"},   {'к', "k"},   {'л', "l"},   {'м', "m"},   {'н', "n"},
+        {'о', "o"},   {'п', "p"},   {'р', "r"},   {'с', "s"},   {'т', "t"},
+        {'у', "u"},   {'ф', "f"},   {'х', "kh"},  {'ц', "ts"},  {'ч', "ch"},
+        {'ш', "sh"},  {'щ', "sch"},{'ъ', ""},    {'ы', "y"},   {'ь', ""},
+        {'э', "e"},   {'ю', "yu"},  {'я', "ya"},
+        {'А', "A"},   {'Б', "B"},   {'В', "V"},   {'Г', "G"},   {'Д', "D"},
+        {'Е', "E"},   {'Ё', "E"},   {'Ж', "Zh"},  {'З', "Z"},   {'И', "I"},
+        {'Й', "Y"},   {'К', "K"},   {'Л', "L"},   {'М', "M"},   {'Н', "N"},
+        {'О', "O"},   {'П', "P"},   {'Р', "R"},   {'С', "S"},   {'Т', "T"},
+        {'У', "U"},   {'Ф', "F"},   {'Х', "Kh"},  {'Ц', "Ts"},  {'Ч', "Ch"},
+        {'Ш', "Sh"},  {'Щ', "Sch"},{'Ъ', ""},    {'Ы', "Y"},   {'Ь', ""},
+        {'Э', "E"},   {'Ю', "Yu"},  {'Я', "Ya"}, {' ', "_"}
+    };
+
 }
